74b5de0f1f5755f67de511257ce244de4e349e0e
Abdullahalfaruk <farukabdullahal9@gmail.com>
2021-10-02 01:58:13 +0600

    site done

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -3,6 +3,7 @@ import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import {addToDb, getStoredCart} from '../../utilities/fakedb';
 import './Shope.css';
+import { Link } from 'react-router-dom';
 
 const Shop = () => {
 
@@ -37,7 +38,17 @@ const Shop = () => {
     }, [products])
 
     const handleAddToCart = (product) =>{
-        const newCart = [...cart, product];
+        const exists = cart.find(pd => pd.key === product.key);
+        let newCart = [];
+        if (exists){
+            const rest= cart.filter(pd => pd.key !== product.key);
+            exists.quantity = exists.quantity + 1;
+            newCart = [...rest, product];
+        }
+        else{
+            product.quantity = 1;
+            newCart = [...cart, product];
+        }
         setCart(newCart);
         addToDb(product.key);
     }
@@ -67,7 +78,11 @@ const Shop = () => {
                 }
             </div>
             <div className="cart-container">
-                <Cart cart={cart}></Cart>
+                <Cart cart={cart}>
+                    <Link to="/review">
+                        <button className="btn-regular">Review Your Order</button>
+                    </Link>
+                </Cart>
             </div>
         </div>
         </div>
